df94c858dbfb26ac351f512f70b271f43ab88b2a
John-Naquin <johnnaquin14@gmail.com>
2025-02-16 19:55:52 -0600

    Changed paper link

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,7 @@ function App() {
             📜 Project Backend Code
           </a>
           <a 
-            href="" 
+            href="https://drive.google.com/file/d/16j9XzBsUiPLAwqyqxQVaW9ZMaBuki2u7/view?usp=sharing" 
             target="_blank" 
             className="text-blue-500 hover:underline"
           >
